a317548d66b2c6a519cffe4bca810fec1942bfc0
Felipe Vega <feli.vega@duocuc.cl>
2023-05-10 17:22:10 -0400

    Api crud

--- Flask-mysql-python/src/app.py
+++ Flask-mysql-python/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, jsonify, make_response, render_template, request, redirect, url_for
 import os 
 import database as db
 
@@ -8,6 +8,8 @@ template_dir = os.path.join(template_dir, 'src', 'templates')
 app = Flask(__name__, template_folder= template_dir)
 
 #RUTAS DE LA APP
+
+#CRUD
 
 @app.route('/')
 def Index():
@@ -22,7 +24,6 @@ def Index():
     cursor.close()
     return render_template('index.html', data = insertObject)
 
-#Ruta para guardar usuarios en la bd
 @app.route('/user', methods=['POST'])
 def addUser():
     nombre = request.form['nombre']
@@ -65,6 +66,70 @@ def edit(id):
         db.database.commit()
     return redirect(url_for('Index'))
     
+    
+#API
+
+@app.route('/api/productos', methods=['GET'])
+def obtener_productos():
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM producto")
+    myresult = cursor.fetchall()
+    #Convertir los datos a lista de diccionarios
+    result = []
+    columNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        result.append(dict(zip(columNames, record)))
+    cursor.close()
+    return jsonify(result)
+
+@app.route('/api/productos', methods=['POST'])
+def create_producto():
+    nombre = request.json['nombre']
+    descripcion = request.json['descripcion']
+    categoria = request.json['categoria']
+    precio = request.json['precio']
+    stock = request.json['stock']
+    cursor = db.database.cursor()
+    sql = "INSERT INTO producto (nombre, descripcion, categoria, precio, stock) VALUES (%s, %s, %s, %s, %s)"
+    data = (nombre, descripcion, categoria, precio, stock)
+    cursor.execute(sql, data)
+    db.database.commit()
+    cursor.close()
+    return make_response(jsonify({'message': 'Producto creado exitosamente'}), 201)
+
+@app.route('/api/productos/<int:id>', methods=['GET'])
+def get_producto(id):
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM producto WHERE id = %s", (id,))
+    producto = cursor.fetchone()
+    cursor.close()
+    if producto is not None:
+        return jsonify(producto)
+    else:
+        return make_response(jsonify({'error': 'Producto no encontrado'}), 404)
+
+@app.route('/api/productos/<int:id>', methods=['PUT'])
+def update_producto(id):
+    nombre = request.json['nombre']
+    descripcion = request.json['descripcion']
+    categoria = request.json['categoria']
+    precio = request.json['precio']
+    stock = request.json['stock']
+    cursor = db.database.cursor()
+    sql = "UPDATE producto SET nombre=%s, descripcion=%s, categoria=%s, precio=%s, stock=%s WHERE id=%s"
+    data = (nombre, descripcion, categoria, precio, stock, id)
+    cursor.execute(sql, data)
+    db.database.commit()
+    cursor.close()
+    return make_response(jsonify({'message': 'Producto actualizado exitosamente'}), 200)
+
+@app.route('/api/productos/<int:id>', methods=['DELETE'])
+def delete_producto(id):
+    cursor = db.database.cursor()
+    cursor.execute("DELETE FROM producto WHERE id = %s", (id,))
+    db.database.commit()
+    cursor.close()
+    return make_response(jsonify({'message': 'Producto eliminado exitosamente'}), 200)
     
     
 if __name__ == '__main__':
