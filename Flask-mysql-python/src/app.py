8ded500a1ec5cc92789817195fa76ab9f2317ec3
Felipe Vega <feli.vega@duocuc.cl>
2023-05-17 21:02:24 -0400

    Api pedidos
    - api pedidos con metodos get y put
    - creacion tabla cliente, y pedidos_producto.

--- Flask-mysql-python/src/app.py
+++ Flask-mysql-python/src/app.py
@@ -67,7 +67,9 @@ def edit(id):
     return redirect(url_for('Index'))
     
     
-#API
+#API PRODUCTOS
+
+# GET TODOS LOS PRODUCTOS
 
 @app.route('/api/productos', methods=['GET'])
 def obtener_productos():
@@ -81,6 +83,9 @@ def obtener_productos():
         result.append(dict(zip(columNames, record)))
     cursor.close()
     return jsonify(result)
+
+
+# POST UN PRODUCTO
 
 @app.route('/api/productos', methods=['POST'])
 def create_producto():
@@ -97,6 +102,9 @@ def create_producto():
     cursor.close()
     return make_response(jsonify({'message': 'Producto creado exitosamente'}), 201)
 
+
+# GET PRODUCTO ESPECIFICO
+
 @app.route('/api/productos/<int:id>', methods=['GET'])
 def get_producto(id):
     cursor = db.database.cursor()
@@ -107,6 +115,9 @@ def get_producto(id):
         return jsonify(producto)
     else:
         return make_response(jsonify({'error': 'Producto no encontrado'}), 404)
+
+
+# PUT ACTUALIZAR UN PRODUCTO
 
 @app.route('/api/productos/<int:id>', methods=['PUT'])
 def update_producto(id):
@@ -123,6 +134,9 @@ def update_producto(id):
     cursor.close()
     return make_response(jsonify({'message': 'Producto actualizado exitosamente'}), 200)
 
+
+# ELIMINAR UN PRODUCTO
+
 @app.route('/api/productos/<int:id>', methods=['DELETE'])
 def delete_producto(id):
     cursor = db.database.cursor()
@@ -132,6 +146,134 @@ def delete_producto(id):
     return make_response(jsonify({'message': 'Producto eliminado exitosamente'}), 200)
     
     
-if __name__ == '__main__':
-    app.run(port = 4000, debug=True)
+#API PEDIDOS
+
+# GET TODOS PEDIDOS
+
+@app.route('/api/pedidos', methods=['GET'])
+def obtener_pedidos():
+    cursor = db.database.cursor()
+    cursor.execute("SELECT p.id AS pedido_id, c.rut AS cliente_rut, p.fecha_pedido, pp.producto_id, pr.nombre AS producto_nombre, pp.cantidad \
+                    FROM pedidos p \
+                    JOIN clientes c ON p.rut_cliente = c.rut  \
+                    JOIN pedido_productos pp ON p.id = pp.pedido_id \
+                    JOIN producto pr ON pp.producto_id = pr.id")
+    result = cursor.fetchall()
+    cursor.close()
     
+    #Convertir los datos a lista de diccionarios
+    pedidos = []
+    for row in result:
+        pedido_id = row[0]
+        cliente_rut = row[1]
+        fecha = row[2]
+        producto_id = row[3]
+        producto_nombre = row[4]
+        cantidad = row[5]
+        
+        # Buscar si el pedido ya existe en la lista
+        pedido_existente = next((pedido for pedido in pedidos if pedido['id'] == pedido_id), None)
+        
+        if pedido_existente:
+            # Agregar el producto y su cantidad al pedido existente
+            pedido_existente['productos'].append({
+                'id': producto_id,
+                'nombre': producto_nombre,
+                'cantidad': cantidad
+            })
+        else:
+            # Crear un nuevo pedido con el producto y su cantidad
+            pedido = {
+                'id': pedido_id,
+                'rut_cliente': cliente_rut,
+                'fecha_pedido': fecha,
+                'productos': [{
+                    'id': producto_id,
+                    'nombre': producto_nombre,
+                    'cantidad': cantidad
+                }]
+            }
+            pedidos.append(pedido)
+    
+    return jsonify(pedidos)
+
+# POST PEDIDOS
+
+from flask import request
+
+@app.route('/api/pedidos', methods=['POST'])
+def crear_pedido():
+    # Obtener los datos del pedido del cuerpo de la solicitud JSON
+    data = request.json
+    rut_cliente = data['rut_cliente']
+    fecha_pedido = data['fecha_pedido']
+    productos = data['productos']
+
+    # Insertar el pedido en la tabla de pedidos
+    cursor = db.database.cursor()
+    sql = "INSERT INTO pedidos (rut_cliente, fecha_pedido) VALUES (%s, %s)"
+    cursor.execute(sql, (rut_cliente, fecha_pedido))
+    db.database.commit()
+
+    # Obtener el ID del pedido recién insertado
+    pedido_id = cursor.lastrowid
+
+    # Insertar los productos del pedido en la tabla de pedido_productos
+    for producto in productos:
+        producto_id = producto['producto_id']
+        cantidad = producto['cantidad']
+        sql = "INSERT INTO pedido_productos (pedido_id, producto_id, cantidad) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (pedido_id, producto_id, cantidad))
+        db.database.commit()
+
+    cursor.close()
+
+    return jsonify({'message': 'Pedido creado exitosamente'}), 201
+
+
+# GET ESPECIFICO PEDIDOS
+
+@app.route('/api/pedidos/<int:id>', methods=['GET'])
+def get_pedido(id):
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM pedidos WHERE id = %s", (id,))
+    pedido = cursor.fetchone()
+    cursor.close()
+    if pedido is not None:
+        return jsonify(pedido)
+    else:
+        return make_response(jsonify({'error': 'Pedido no encontrado'}), 404)
+
+
+# PUT PEDIDO EDITAR
+
+@app.route('/api/pedidos/<int:id>', methods=['PUT'])
+def update_pedido(id):
+    cantidad = request.json['cantidad']
+    producto_id = request.json['producto_id']
+    cursor = db.database.cursor()
+    sql = "UPDATE pedidos SET cantidad=%s, producto__id=%s WHERE id=%s"
+    data = (cantidad, producto_id, id)
+    cursor.execute(sql, data)
+    db.database.commit()
+    cursor.close()
+    return make_response(jsonify({'message': 'Pedido actualizado exitosamente'}), 200)
+
+# DELETE PEDIDO
+
+@app.route('/api/pedidos/<int:id>', methods=['DELETE'])
+def delete_pedido(id):
+    cursor = db.database.cursor()
+    cursor.execute("DELETE FROM pedidos WHERE id = %s", (id,))
+    db.database.commit()
+    cursor.close()
+    return make_response(jsonify({'message': 'Pedido eliminado exitosamente'}), 200)
+
+
+if __name__ == '__main__':  
+    app.run(port = 4000, debug=True)
+
+# stock
+# armar json
+# validar stock en productos
+# guardar en tabla de pedidos
